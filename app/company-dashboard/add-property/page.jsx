bb8d3bda59a63575388c8fee7af49b3688f8d5df
metromindz-dev-team <development@metromindz.com>
2025-12-10 14:27:54 +0530

    resolved document is not defibed in add-propery page

--- app/company-dashboard/add-property/page.jsx
+++ app/company-dashboard/add-property/page.jsx
@@ -2,10 +2,10 @@
 import AddProperty from "@/components/company-dashboard/AddProperty";
 import React from "react";
 
-export const metadata = {
-  title: "Add Property || Proty - Real Estate React Nextjs Template",
-  description: "Proty - Real Estate React Nextjs Template",
-};
+// export const metadata = {
+//   title: "Add Property || Proty - Real Estate React Nextjs Template",
+//   description: "Proty - Real Estate React Nextjs Template",
+// };
 export default function page() {
   return (
     <>
